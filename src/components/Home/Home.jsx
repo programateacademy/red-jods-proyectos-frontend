6d0c3f37d5e75cd27275bd3e323118ef9fb50999
valentinapuentesgarzon <valentinapuentes@gmail.com>
2023-03-08 10:28:06 -0500

    add pagination to Home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Box from '@mui/material/Box';
 import Decoración from '/Decoración.png'
 import Card from '@mui/material/Card';
@@ -8,9 +8,19 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import { ODS } from './Data';
 import './Home.css'
+import Pagination from '@mui/material/Pagination';
 
 
 export default function Home() {
+
+    const [page, setPage] = useState (1)
+
+
+    const handleChange = (e, p) => {
+        console.log(e,p)
+        setPage(p)
+    }
+
     return (
         <div>
             {/* This elements are displayed when screen is medium or large */}
@@ -27,26 +37,34 @@ export default function Home() {
                 <img src={Decoración} alt="" style={{ padding: "0 0 10px 10px", maxWidth: "280px" }} />
             </Box>
             <div className="cardBox">
-                {ODS.map (item => {return (
+                {ODS.map(item => {
+                    return (
 
-                <Card id='Card' sx={{ maxWidth: 300 }}>
-                    <CardMedia
-                        sx={{ height: 300}}
-                        image={item.img}
-                        title={item.Name}
-                    />
-                    <CardContent>
-                        <h2>{item.Name}</h2>
-                        <h3>{item.ODS}</h3>
-                    </CardContent>
-                    <CardActions>
-                        <Button id='Btn1' variant="contained">Ver Proyecto</Button>
-                        <Button id='Btn2' variant="outlined">Editar</Button>
-                    </CardActions>
-                </Card>
-    
-                )})}
+                        <Card id='Card' sx={{ maxWidth: 300 }}>
+                            <CardMedia
+                                sx={{ height: 300 }}
+                                image={item.img}
+                                title={item.Name}
+                            />
+                            <CardContent>
+                                <h2>{item.Name}</h2>
+                                <h3>{item.ODS}</h3>
+                            </CardContent>
+                            <CardActions id='actionsBox'>
+                                <Button id='Btn1' variant="contained">Ver Proyecto</Button>
+                                <Button id='Btn2' variant="outlined">Editar</Button>
+                            </CardActions>
+                        </Card>
+
+                    )
+                })}
             </div>
+
+            <h1>Current page is {page}</h1>
+            
+            <Pagination count={10} onChange={handleChange} color="primary" />
+              
+            
         </div>
     )
 }
---
+++ src/components/Home/pagination/pagination.jsx
@@ -0,0 +1,5 @@
+export default function AppPagination () {
+    return (
+        <h1>Pagination</h1>
+    );
+};
