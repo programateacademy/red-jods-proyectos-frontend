ab088049380e86c9ee218178bf475648c0da2a90
Laura Ximena Velasco Rocha <12ximenavelasco@gmail.com>
2023-03-16 12:52:26 -0500

    media queries home check

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,28 +10,30 @@ import AppPagination from './pagination/pagination';
 import { useState } from 'react';
 
 
+
 export default function Home() {
 
     const [card, setCard] = useState([]);
 
     return (
-        <div>
+        <div className='container_box' >
             {/* This elements are displayed when screen is medium or large */}
             <Box sx={{ display: { xs: 'none', md: 'block' } }}>
                 <h1 style={{ padding: "40px 40px 0px 40px" }}>¡Hola Súper Administrador!</h1>
-                <img src={Decoración} alt="" style={{ padding: "0 0 40px 40px" }} />
+                <img src={Decoración} alt="" style={{ padding: "0 0 40px 40px", width: "450px" }} />
+                
             </Box>
 
 
 
             {/* This elements are displayed when screen is small */}
-            <Box sx={{ display: { xs: 'block', md: 'none' } }}>
+            <Box  sx={{ display: { xs: 'block', md: 'none' } }}>
                 <h1 style={{ padding: "10px 10px 0px 10px" }}>¡Hola Súper Administrador!</h1>
                 <img src={Decoración} alt="" style={{ padding: "0 0 10px 10px", maxWidth: "280px" }} />
             </Box>
             <div className="cardBox">
                 {card.map(item => {
-                    return (
+                    return ( 
 
                         <Card key={item.id} id='Card' sx={{ maxWidth: 280 }}>
                             <CardMedia
