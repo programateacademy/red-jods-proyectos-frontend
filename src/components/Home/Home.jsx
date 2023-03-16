0d54f3a5837c0815420217e7806e70454b9c354f
Laura Ximena Velasco Rocha <12ximenavelasco@gmail.com>
2023-03-16 13:21:29 -0500

    media queries home recontracheck

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -20,7 +20,7 @@ export default function Home() {
             {/* This elements are displayed when screen is medium or large */}
             <Box sx={{ display: { xs: 'none', md: 'block' } }}>
                 <h1 style={{ padding: "40px 40px 0px 40px" }}>¡Hola Súper Administrador!</h1>
-                <img src={Decoración} alt="" style={{ padding: "0 0 40px 40px", width: "450px" }} />
+                <img src={Decoración} alt="" style={{ padding: "0 0 40px 40px", maxWidth: "480px" }} />
                 
             </Box>
 
@@ -29,7 +29,7 @@ export default function Home() {
             {/* This elements are displayed when screen is small */}
             <Box  sx={{ display: { xs: 'block', md: 'none' } }}>
                 <h1 style={{ padding: "10px 10px 0px 10px" }}>¡Hola Súper Administrador!</h1>
-                <img src={Decoración} alt="" style={{ padding: "0 0 10px 10px", maxWidth: "280px" }} />
+                <img src={Decoración} alt="" style={{ padding: "0 0 10px 10px", maxWidth: "320px" }} />
             </Box>
             <div className="cardBox">
                 {card.map(item => {
